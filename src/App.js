82be5a172648ab9b100e5210880a734346fffc7b
Arnab Hazra <harnab28@gmail.com>
2020-09-03 21:06:11 +0530

    git component part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand} from 'reactstrap'
+import Menu from './components/MenuComponent';
 
 class App extends Component{
   render(){
@@ -11,9 +11,10 @@ class App extends Component{
             <div className="container">
               <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>  
             </div>
-
           </Navbar>
+          <Menu/>
       </div>
+      
     );
   }
 }
